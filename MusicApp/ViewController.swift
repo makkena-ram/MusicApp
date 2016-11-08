c43a40efd3b192ea7be90cd86d6a868e794c41e6
Rama Personal <makkena.ram538@gmail.com>
2016-11-08 15:36:07 -0600

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Rama Personal on 11/8/16.
+//  Copyright © 2016 Rama Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
